9d5637caba4bc1d8fdd622e0880d5be7c3d1d354
Florent DAEM <florent.daem@gmail.com>
2023-10-04 22:56:32 +0200

    temps limité

--- light wave 2D.py
+++ light wave 2D.py
@@ -6,15 +6,15 @@ mode = 'intensite'  # 'vecteur' ou 'intensite'
 
 # Paramètres de la simulation
 n_steps = 200  # Nombre d'itérations temporelles
-n_points_x = 300  # Nombre de points en x
+n_points_x = 100  # Nombre de points en x
 n_points_y = n_points_x  # Nombre de points en y
-c = 14.0  # Vitesse de la lumière (en m/s)
+c = 15.0  # Vitesse de la lumière (en m/s)
 dx = 0.1  # Espacement spatial en x
 dy = dx  # Espacement spatial en y
 dt = 0.005  # Pas de temps
 
-epsilon_0 = 8.8541878128 * 10**-12 # Constante diélectrique du vide (en F/m = C/(V.m))
-e = 1.602176634 * 10**-19  # Charge élémentaire (en C)
+epsilon_0 = 1 # Constante diélectrique du vide (en F/m = C/(V.m))
+e = 1 # Charge élémentaire (en C)
 
 # Calcul du paramètre de stabilité CFL
 stability_param = c * dt / (dx)
@@ -45,8 +45,8 @@ if stability_param < 1.0:
 
     # Coefficients d'absorption (2D)
     alpha_values = np.zeros((n_points_x, n_points_y))
-    alpha_values[:n_points_x // 2, :] = 0  # Première moitié en x avec une absorption nulle
-    alpha_values[n_points_x // 2:, :] = 0.1  # Deuxième moitié en x avec une absorption non nulle
+    alpha_values[:n_points_x // 2, :] = 0.1  # Première moitié en x avec une absorption nulle
+    alpha_values[n_points_x // 2:, :] = 0.3  # Deuxième moitié en x avec une absorption non nulle
 
     # Création de la figure
     fig, ax = plt.subplots(figsize=(8, 8))
@@ -85,17 +85,17 @@ if stability_param < 1.0:
                 Ex[i + 1],
                 Ey[i + 1],
                 color='r',
-                scale=10**-6,
+                scale=None,
                 minlength=0
             )
             return fleche,
         # Affichage de l'intensité
         elif mode == 'intensite':
-            intensite = ax.imshow(I, extent=[0, n_points_x, 0, n_points_y], origin='lower', cmap='viridis', vmin=0, vmax=10**-15)
+            intensite = ax.imshow(I, extent=[0, n_points_x, 0, n_points_y], origin='lower', cmap='viridis', vmin=0)
             return intensite,
 
     # Animation
-    ani = FuncAnimation(fig, animate, frames=n_steps - 1, interval=50, blit=True)
+    ani = FuncAnimation(fig, animate, frames=n_steps - 1, interval=50, blit=True, repeat=False)
 
     ax.set_xlabel('Position en x')
     ax.set_ylabel('Position en y')
